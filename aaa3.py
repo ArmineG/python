e01d69429c5a74c9e2b749aa219db02bdbcdbf3d
ArmineG <43245207+ArmineG@users.noreply.github.com>
2019-10-07 21:01:20 +0400

    Add files via upload

---
+++ aaa3.py
@@ -0,0 +1,7 @@
+my_string = "this is MY test string!"
+print(my_string)
+print(my_string.capitalize())
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.title())
+print(my_string.replace("MY", "YOUR"))
