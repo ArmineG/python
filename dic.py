9d783d55a4e73cdcc8f12f5439f0676e7de32eed
ArmineG <43245207+ArmineG@users.noreply.github.com>
2019-10-14 19:50:23 +0400

    Add files via upload

---
+++ dic.py
@@ -0,0 +1,7 @@
+human = {"name": "jirayr", "surname": "Melikyan", "age": 19, 'is_marrid':False}
+print(human)
+human['age'] = 30
+print(human)
+human['have_pet'] = False
+del human['age']
+print(human)
